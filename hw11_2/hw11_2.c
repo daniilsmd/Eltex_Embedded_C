3f399b7562320647246db712d090444f1c6088de
daniilsmd <150523577+daniilsmd@users.noreply.github.com>
2024-06-28 17:39:51 +0700

    Update hw11_2.c

--- hw11_2/hw11_2.c
+++ hw11_2/hw11_2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "threadsum.h"
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
+
+int main() {
+
+	pthread_t theard[5];
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++) {
+		pthread_create(&theard[i], NULL, threadsum, &sum);
+		printf("Создание %d - ого потока\n", i);
+	}
+	for (int j = 0; j < 5; j++){
+		pthread_join(theard[j], NULL);
+	}
+	printf("Сумма: %d\n", sum);
+}
