a0a07d2de46e4c4b528113754eb083fb41476ede
daniilsmd <150523577+daniilsmd@users.noreply.github.com>
2024-07-02 23:11:43 +0700

    Update includes.h

--- 16.6_ETHERNET/includes.h
+++ 16.6_ETHERNET/includes.h
@@ -12,4 +12,33 @@
 #ifndef __INCLUDES_H__
 #define __INCLUDES_H__
 
+struct udp_header {
+    short src_port;
+    short des_port;
+    short length;
+    short check_sum;
+    char payload[47-42];
+};
+
+struct ip_header {
+    char Ver_IHL;     
+    char DS;
+    short length;
+    short identification;
+    short flags_offset;
+    char TTL; 
+    char Transport_Proto;
+    short Checksum;
+    int src_ip;
+    int dest_ip; 
+    struct udp_header payload; 
+};
+
+struct ethernet_header {
+    char des_mac[6];
+    char src_mac[6];
+    short type;
+    struct ip_header payload;
+};
+
 #endif
